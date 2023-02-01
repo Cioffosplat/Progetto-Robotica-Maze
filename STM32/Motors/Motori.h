60447d52fefb06f7c114ff0c4d8941187fdc136d
Filippo Schierato <filippo.schierato@itiszuccante.edu.it>
2023-02-01 09:06:07 +0100

    Fixed Motors
    Fixed files for motors to work with a single h bridge

--- STM32/Motors/Motori.h
+++ STM32/Motors/Motori.h
@@ -3,45 +3,36 @@
 
 #include <Arduino.h>
 
+// ALL of the commentated methods inside of the class are used to connect
+// the enable pins and regulate the motor's speed and rpm
+
 class Motori
 {
 private:
     int d1;
     int d2;
-    int motored_b;
+    //int motored_b;
     int d3;
     int d4;
-    int motored_a;
-    int s1;
-    int s2; 
-    int motores_a;
-    int motores_b;
-    int s3;
-    int s4;
-    int defaultSpeed;
+    //int motored_a;
+    //int defaultSpeed;
 
-    int chooseSpeed(int speed);
+    //int chooseSpeed(int speed);
 public:
 
     Motori(
         int d1,
         int d2,
-        int motored_b,
+        //int motored_b,
         int d3,
         int d4,
-        int motored_a,
-        int s1,
-        int s2,
-        int motores_a,
-        int motores_b,
-        int s3,
-        int s4,
-        int defaultSpeed
+        //int motored_a,
+        //int defaultSpeed
     );
-    void avanti(int speed = -1);
-    void indietro(int speed = -1);
-    void destra(int speed = -1);
-    void sinistra(int speed = -1);
+    void avanti(/*int speed = -1*/);
+    void indietro(/*int speed = -1*/);
+    void destra(/*int speed = -1*/);
+    void sinistra(/*int speed = -1*/);
     void fermo();
 }; 
 
