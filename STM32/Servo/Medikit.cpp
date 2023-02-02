179ce5f4501864f57be0eb17ea77ab2d35edb03d
getalSemenz <giulio.semenzato05@gmail.com>
2023-02-02 22:57:47 +0100

    add the servo_medikit library

---
+++ STM32/Servo/Medikit.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Giulio on 02/02/2023.
+//
+
+#include "Medikit.h"
+
+#define SERVO_PIN B5
+#define CLOSED 0
+#define OPEN 90
+
+void setupServo(){
+}
+
+void servoDrop(){
+
+}
---
+++ STM32/Servo/Medikit.h
@@ -0,0 +1,16 @@
+//
+// Created by Giulio on 02/02/2023.
+//
+
+#ifndef MEDIKIT_H
+#define MEDIKIT_H
+
+#include <Servo.h>
+#include <Arduino.h>
+
+    void setupServo();
+    void servoDrop();
+    Servo servo;
+
+
+#endif MEDIKIT_H
