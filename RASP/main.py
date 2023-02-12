05d96faf9298fbc8ab4ee28cb1121cc61dca9654
getalSemenz <giulio.semenzato05@gmail.com>
2023-02-12 17:27:45 +0100

    initial write of the Serialc_ommands.txt

--- RASP/main.py
+++ RASP/main.py
@@ -1,9 +1,16 @@
 import serial
 
+def isWall(millis):
+  print("Hello from a function")
+
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
     ser.reset_input_buffer()
-    while True:
+    condition = True
+
+    while condition:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
+
+
