7b4f3225b62b131a6cf19ef6cc8e9b2b3fbccdf6
getalSemenz <giulio.semenzato05@gmail.com>
2023-02-14 00:32:46 +0100

    based raspberry main

--- RASP/main.py
+++ RASP/main.py
@@ -1,16 +1,41 @@
 import serial
+import time
 
-def isWall(millis):
-  print("Hello from a function")
+from Settings import const_distaces
+
+L_frontUp = 0
+L_frontDown = 1
+L_right = 2
+L_left = 3
+L_back = 4
+
+def isNotWall(millis):
+    if  millis < const_distaces.WALL_MAX:
+        return False
+    else:
+        return True
+
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=5)
     ser.reset_input_buffer()
     condition = True
-
     while condition:
+        ser.write("3\n".encode('utf-8'))
         if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(line)
-
+            line = (ser.readline().decode('utf-8').rstrip())
+            lasers = line.split(";")
+            if isNotWall(lasers[L_right]):
+                ser.write("12\n".encode('utf-8'))
+            elif isNotWall(lasers[L_frontDown]):
+                ser.write("10\n".encode('utf-8'))
+            elif isNotWall(lasers[L_left]):
+                ser.write("13\n".encode('utf-8'))
+            else:
+                ser.write("11\n".encode('utf-8'))
+            while ser.in_waiting == 0:
+                time.sleep(0.5)
+            line = (ser.readline().decode('utf-8').rstrip())
+            if line == "11":
+                time.sleep(5)
 
