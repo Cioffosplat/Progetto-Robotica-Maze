34ea3ead7179d1bc3e0c49971c134bdfd257acc6
getalSemenz <giulio.semenzato05@gmail.com>
2023-02-22 22:25:58 +0100

    changes in raspberry main

--- RASP/main.py
+++ RASP/main.py
@@ -20,22 +20,57 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=5)
     ser.reset_input_buffer()
     condition = True
+    lasers = []
+    while condition:
+        while ser.in_waiting == 0:
+            time.sleep(0.001)
+        line = (ser.readline().decode('utf-8').rstrip())
+        print(line)
+        if line == "READY":
+            condition = False
+    condition = True
     while condition:
         ser.write("3\n".encode('utf-8'))
-        if ser.in_waiting > 0:
-            line = (ser.readline().decode('utf-8').rstrip())
-            lasers = line.split(";")
-            if isNotWall(lasers[L_right]):
-                ser.write("12\n".encode('utf-8'))
-            elif isNotWall(lasers[L_frontDown]):
-                ser.write("10\n".encode('utf-8'))
-            elif isNotWall(lasers[L_left]):
-                ser.write("13\n".encode('utf-8'))
-            else:
-                ser.write("11\n".encode('utf-8'))
+        print(3)
+        lasers = []
+        for i in range(5):
             while ser.in_waiting == 0:
-                time.sleep(0.5)
+                time.sleep(0.001)
             line = (ser.readline().decode('utf-8').rstrip())
-            if line == "11":
-                time.sleep(5)
+            print("laser " + i + " = " + line)
+            lasers.append(line)
+        if isNotWall(lasers[L_right]):
+            print("DESTRA")
+            ser.write("12\n".encode('utf-8'))
+            if not isNotWall(lasers[L_left]):
+                ser.write("15\n".encode('utf-8'))
+            ser.write("10\n".encode('utf-8'))
+        elif isNotWall(lasers[L_frontDown]):
+            print("AVANTI")
+            ser.write("10\n".encode('utf-8'))
+        elif isNotWall(lasers[L_left]):
+            print("SINISTRA")
+            ser.write("13\n".encode('utf-8'))
+            if not isNotWall(lasers[L_right]):
+                ser.write("15\n".encode('utf-8'))
+            ser.write("10\n".encode('utf-8'))
+        elif isNotWall(lasers[L_back]):
+            print("INDIETRO")
+            ser.write("12\n".encode('utf-8'))
+            ser.write("15\n".encode('utf-8'))
+            ser.write("12\n".encode('utf-8'))
+            ser.write("15\n".encode('utf-8'))
+            ser.write("10\n".encode('utf-8'))
+        while ser.in_waiting == 0:
+            time.sleep(0.001)
+        line = (ser.readline().decode('utf-8').rstrip())
+        print(line)
+        if line == "0":
+            ser.write("12\n".encode('utf-8'))
+            ser.write("15\n".encode('utf-8'))
+            ser.write("12\n".encode('utf-8'))
+            ser.write("15\n".encode('utf-8'))
+            ser.write("10\n".encode('utf-8'))
+        if line == "11":
+            time.sleep(5)
 
