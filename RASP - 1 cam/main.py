abf4d4b443dafd258485234b718eaca786cc08c6
getalSemenz <giulio.semenzato05@gmail.com>
2023-02-24 16:47:31 +0100

    first try for cam main

--- RASP - 1 cam/main.py
+++ RASP - 1 cam/main.py
@@ -12,70 +12,90 @@ L_right = 2
 L_left = 3
 L_back = 4
 
+cum1 = Camera(2)
 
-
-def isNotWall(millis):
+def isWall(millis):
     if  millis < const_distaces.WALL_MAX:
-        return False
-    else:
         return True
+    else:
+        return False
+
+
+def readCum():
+    result = read_all(cum1)
+    print("cum result:")
+    print(result)
+
+
+def robotSinastra():
+    ser.write("13\n".encode('utf-8'))
+    if isWall(lasers[L_right]):
+        ser.write("15\n".encode('utf-8'))
+
+
+def robotDestra():
+    ser.write("12\n".encode('utf-8'))
+    if isWall(lasers[L_left]):
+        ser.write("15\n".encode('utf-8'))
+
+
+def getLasers():
+    ser.write("3\n".encode('utf-8'))
+    print("get lasers")
+    lasers = []
+    for i in range(5):
+        while ser.in_waiting == 0:
+            time.sleep(0.001)
+        line = float((ser.readline().decode('utf-8').rstrip()))
+        print("laser = " + str(line))
+        lasers.append(line)
+    return lasers
+
+
+def robotAvanti():
+    ser.write("10\n".encode('utf-8'))
+
+
+def robotIndietro():
+    ser.write("12\n".encode('utf-8'))
+    ser.write("15\n".encode('utf-8'))
+    ser.write("12\n".encode('utf-8'))
+    ser.write("15\n".encode('utf-8'))
+    ser.write("10\n".encode('utf-8'))
 
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=5)
-    ser.reset_input_buffer()
-    condition = True
-    lasers = []
+    condition = False
+    while not condition:
+        try:
+            ser = serial.Serial('/dev/ttyACM0', 115200, timeout=5)
+            ser.reset_input_buffer()
+            condition = True
+        except:
+            print("Serial waiting")
     while condition:
-        while ser.in_waiting == 0:
-            time.sleep(0.001)
-        line = (ser.readline().decode('utf-8').rstrip())
-        print(line)
-        if line == "READY":
-            condition = False
-    condition = True
-    while condition:
-        ser.write("3\n".encode('utf-8'))
-        print(3)
-        lasers = []
-        for i in range(5):
-            while ser.in_waiting == 0:
-                time.sleep(0.001)
-            line = (ser.readline().decode('utf-8').rstrip())
-            print("laser " + i + " = " + line)
-            lasers.append(line)
-        if isNotWall(lasers[L_right]):
+        readCum()
+        lasers = getLasers()
+        if not isWall(lasers[L_right]):
             print("DESTRA")
-            ser.write("12\n".encode('utf-8'))
-            if not isNotWall(lasers[L_left]):
-                ser.write("15\n".encode('utf-8'))
-            ser.write("10\n".encode('utf-8'))
-        elif isNotWall(lasers[L_frontDown]):
+            robotDestra()
+            robotAvanti()
+        elif not isWall(lasers[L_frontUp]):
             print("AVANTI")
-            ser.write("10\n".encode('utf-8'))
-        elif isNotWall(lasers[L_left]):
+            robotAvanti()
+        elif not isWall(lasers[L_left]):
             print("SINISTRA")
-            ser.write("13\n".encode('utf-8'))
-            if not isNotWall(lasers[L_right]):
-                ser.write("15\n".encode('utf-8'))
-            ser.write("10\n".encode('utf-8'))
-        elif isNotWall(lasers[L_back]):
+            robotSinastra()
+            robotAvanti()
+        elif not isWall(lasers[L_back]):
             print("INDIETRO")
-            ser.write("12\n".encode('utf-8'))
-            ser.write("15\n".encode('utf-8'))
-            ser.write("12\n".encode('utf-8'))
-            ser.write("15\n".encode('utf-8'))
-            ser.write("10\n".encode('utf-8'))
+            robotIndietro()
         while ser.in_waiting == 0:
             time.sleep(0.001)
         line = (ser.readline().decode('utf-8').rstrip())
         print(line)
         if line == "0":
-            ser.write("12\n".encode('utf-8'))
-            ser.write("15\n".encode('utf-8'))
-            ser.write("12\n".encode('utf-8'))
-            ser.write("15\n".encode('utf-8'))
-            ser.write("10\n".encode('utf-8'))
+            robotIndietro()
         if line == "11":
             time.sleep(5)
 
