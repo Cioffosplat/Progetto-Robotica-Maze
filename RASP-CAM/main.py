abf9b0f04f81b31a709b2eba5f872fbea74f1fd0
Filippo Schierato <filippo.schierato@itiszuccante.edu.it>
2023-02-28 00:48:18 +0100

    Updated Main
    added gyro in main

--- RASP-CAM/main.py
+++ RASP-CAM/main.py
@@ -1,10 +1,17 @@
 import serial
 import time
+import math
 
+from mpu6050 import mpu6050
 from camera import Camera
 from analysis import read_all
 
 from Settings import const_distaces
+
+# Initialize MPU6050 sensor
+sensor = mpu6050(0x68)
+# Define constant for MPU6050
+RAD_TO_DEG = 57.295779513082320876798154814105
 
 L_frontUp = 0
 L_frontDown = 1
@@ -16,6 +23,23 @@ L_back = 4
 busses = Camera.list_cameras()
 l_camera = Camera('/dev/video0', right=False)
 r_camera = Camera('/dev/video2', right=True)
+
+def get_pitch():
+    accel_data = sensor.get_accel_data()
+    x = accel_data['x']
+    y = accel_data['y']
+    z = accel_data['z']
+    pitch = math.atan2(-x, math.sqrt(y*y + z*z))
+    pitch_deg = pitch * RAD_TO_DEG
+    if pitch_deg > 180:
+        pitch_deg = pitch_deg - 360
+    return pitch_deg
+
+# Main loop example to implement in the code
+#while True:
+    #pitch = get_pitch()
+    #print("Pitch: {:.2f}".format(pitch))
+    #time.sleep(0.1)
 
 def isWall(millis):
     if  millis < const_distaces.WALL_MAX:
