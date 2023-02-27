32c55eb30d4c6da048fd0ef884cc4f651b775757
Filippo Schierato <filippo.schierato@itiszuccante.edu.it>
2023-02-27 22:17:37 +0100

    Gryo
    added gyro test file

---
+++ RASP-CAM/Gyro/Gyro.py
@@ -0,0 +1,27 @@
+from mpu6050 import mpu6050
+import math
+import time
+
+# Initialize MPU6050 sensor
+sensor = mpu6050(0x68)
+
+# Define constants
+RAD_TO_DEG = 57.295779513082320876798154814105
+
+# Define a function to calculate pitch angle
+def get_pitch():
+    accel_data = sensor.get_accel_data()
+    x = accel_data['x']
+    y = accel_data['y']
+    z = accel_data['z']
+    pitch = math.atan2(-x, math.sqrt(y*y + z*z))
+    pitch_deg = pitch * RAD_TO_DEG
+    if pitch_deg < 0:
+        pitch_deg = 360 + pitch_deg
+    return pitch_deg
+
+# Main loop
+while True:
+    pitch = get_pitch()
+    print("Pitch: {:.2f}".format(pitch))
+    time.sleep(0.1)
