f02aac4ad173b25690da06a259dd9b5e12095dff
Filippo Schierato <filippo.schierato@itiszuccante.edu.it>
2023-03-15 15:55:48 +0100

    removed useless stuff

--- STM32Testing/Gyro/Gyro.ino
+++ STM32Testing/Gyro/Gyro.ino
@@ -3,13 +3,9 @@
 #include "Giroscopio.h"
 
 Giroscopio *giro;
-<<<<<<< HEAD
-  
-=======
 
 float angolo;
 
->>>>>>> 6d2cf53d9af8d581071a71f990d9107a22e138af
 void setup() {
   Wire.begin();
   Serial.begin(115200);
@@ -18,16 +14,13 @@ void setup() {
 
 void loop() {
   angolo = giro->getGradi();
-<<<<<<< HEAD
   Serial.print("ZAngle is;");
   Serial.println(angolo);
   delay(100);
-=======
   if(Serial.available()>0){
     while(Serial.available()>0){
       Serial.read();
     }
     Serial.println(angolo);
   }
->>>>>>> 6d2cf53d9af8d581071a71f990d9107a22e138af
 }
