44bb0609279d0a79aae2660e16f0f3ee55a17500
Filippo Schierato <filippo.schierato@itiszuccante.edu.it>
2023-04-17 16:21:18 +0200

    daghe
    Co-Authored-By: Giulio Semenzato <121291090+getalSemenz@users.noreply.github.com>

--- RASP/testSerial.py
+++ RASP/testSerial.py
@@ -6,9 +6,16 @@ if __name__ == '__main__':
 
     arduino = serial.Serial('/dev/ttyUSB0', 57600, timeout=1)  # USB0 == Arduino Nano
     arduino.reset_input_buffer()
+
+    arduino.write("3\n".encode('utf-8'))
+    while arduino.in_waiting == 0:
+        time.sleep(0.001)
+    line = (arduino.readline().decode('utf-8').rstrip())
+    print(line)
+
     print("first reset")
     arduino.setDTR(False)
-    time.sleep(0.1)
+    time.sleep(0.5)
     arduino.setDTR(True)
     arduino.setRTS(False)
     arduino.setRTS(True)
