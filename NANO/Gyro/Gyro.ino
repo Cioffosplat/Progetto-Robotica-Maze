176b60c0400145a9269a0b65f59ef6f47d2fbb16
Filippo Schierato <filippo.schierato@itiszuccante.edu.it>
2023-04-18 00:04:39 +0200

    added new nano code

--- NANO/Gyro/Gyro.ino
+++ NANO/Gyro/Gyro.ino
@@ -2,10 +2,16 @@
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "Giroscopio.h"
 
+MPU6050 mpu;
+
 Giroscopio *giro;
 void setup() {
   Wire.begin();
-  Serial.begin(9600);
+  Serial.begin(57600);
+  mpu.initialize();
+  mpu.dmpInitialize();
+  mpu.setDMPEnabled(true);
+  Serial.println("MPU6050 initialized.");
   giro = new Giroscopio();
 }
 
@@ -13,4 +19,20 @@ void loop() {
   float angolo;
   angolo = giro->getGradi();
   Serial.println(angolo);
-}
+
+  // Wait for MPU6050 data ready interrupt
+  while (!mpu.getIntStatus() & 0x02);
+
+  // Get the latest DMP packet
+  uint8_t fifoBuffer[64];
+  mpu.getFIFOBytes(fifoBuffer, 64);
+  // Get the acceleration data from the packet
+  int16_t ax = ((fifoBuffer[0] << 8) | fifoBuffer[1]);
+
+  // Map the X-axis acceleration to a range of -180 to 180 degrees
+  float x_angle = map(ax, -32768, 32767, -180, 180);
+
+  Serial.print("X-axis angle: ");
+  Serial.println(x_angle);
+  delay(100);
+} 
