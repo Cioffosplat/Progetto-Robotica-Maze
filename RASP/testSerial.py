719a2ad1df78291cf73ead241103a3c1e38675dd
Filippo Schierato <filippo.schierato@itiszuccante.edu.it>
2023-04-17 16:28:23 +0200

    vai
    Co-Authored-By: Giulio Semenzato <121291090+getalSemenz@users.noreply.github.com>

--- RASP/testSerial.py
+++ RASP/testSerial.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
     arduino = serial.Serial('/dev/ttyUSB0', 57600, timeout=1)  # USB0 == Arduino Nano
     arduino.reset_input_buffer()
 
-    arduino.write("3\n".encode('utf-8'))
+    arduino.write("0".encode('utf-8'))
+    print("sent")
     while arduino.in_waiting == 0:
         time.sleep(0.001)
     line = (arduino.readline().decode('utf-8').rstrip())
