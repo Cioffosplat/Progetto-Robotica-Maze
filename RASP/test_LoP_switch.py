cbdfc75e32d39df3e704486316fce4e46c83cd17
getalSemenz <giulio.semenzato05@gmail.com>
2023-04-19 00:41:11 +0200

    Update test_LoP_switch.py

--- RASP/test_LoP_switch.py
+++ RASP/test_LoP_switch.py
@@ -1,18 +1,44 @@
+import serial
 import time
 from RPi import GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
 
+serSTM = serial.Serial('/dev/ttyACM0', 115200, timeout=2)  # ACM0 == STM32F103C8
+serSTM.reset_input_buffer()
+serNano = serial.Serial('/dev/ttyUSB0', 57600, timeout=1)  # USB0 == Arduino Nano
+serNano.reset_input_buffer()
+
 def isLack():
-    if GPIO.input(23):
+    if not GPIO.input(23):
         return True
     else:
         return False
     
+
+def resetNano():
+    serNano.setDTR(False)
+    time.sleep(0.5)
+    serNano.setDTR(True)
+    serNano.setRTS(False)
+    serNano.setRTS(True)
+
+def resetStm():
+    serSTM.setDTR(False)
+    time.sleep(0.5)
+    serSTM.setDTR(True)
+    serSTM.setRTS(False)
+    serSTM.setRTS(True)
+
 if __name__ == '__main__':
     while True:
         if isLack():
             print("ocio che xe lack")
+            resetNano()
+            print("Nano resettato")
+            resetStm()
+            print("STM resettato")
         else:
             print("tutto regolare boss")
+        time.sleep(0.1)
