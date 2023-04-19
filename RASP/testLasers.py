28f2e9d8af481b1450b854bfa5aa4c9784257d5d
getalSemenz <giulio.semenzato05@gmail.com>
2023-04-20 01:11:41 +0200

    Create testLasers.py

---
+++ RASP/testLasers.py
@@ -0,0 +1,32 @@
+import serial
+import time
+
+L_front_R = 0
+L_front_L = 1
+L_left_L = 2
+L_back_L = 3
+L_back_R = 4
+L_right_R = 5
+
+laserName = ["L_front_R", "L_front_L", "L_left_L", "L_back_L", "L_back_R", "L_right_R"]
+
+print("Serial STM")
+serSTM = serial.Serial('/dev/ttyACM0', 115200, timeout=2)  # ACM0 == STM32F103C8
+serSTM.reset_input_buffer()
+
+def getLasers():
+    serSTM.write("3\n".encode('utf-8'))
+    print("get lasers")
+    lasers = []
+    for i in range(6):
+        while serSTM.in_waiting == 0:
+            time.sleep(0.002)
+        line = float((serSTM.readline().decode('utf-8').rstrip()))
+        print(laserName[i] + ": " + str(line))
+        lasers.append(line)
+    return lasers
+
+if __name__ == '__main__':
+    while True:
+        l = getLasers()
+        print(l[:])
