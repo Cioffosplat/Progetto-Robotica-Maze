b930ac81ebec94080b804e0d1a9bb0a88484efaa
getalSemenz <giulio.semenzato05@gmail.com>
2023-04-20 01:27:25 +0200

    Update testLasers.py

--- RASP/testLasers.py
+++ RASP/testLasers.py
@@ -8,7 +8,7 @@ L_back_L = 3
 L_back_R = 4
 L_right_R = 5
 
-laserName = ["L_front_R", "L_front_L", "L_left_L", "L_back_L", "L_back_R", "L_right_R"]
+laserName = ["L_front_R", "L_front_L", "L_left_L", "L_back_L", "L_back_R", "L_right_R", "extra"]
 
 print("Serial STM")
 serSTM = serial.Serial('/dev/ttyACM0', 115200, timeout=2)  # ACM0 == STM32F103C8
@@ -18,12 +18,12 @@ def getLasers():
     serSTM.write("3\n".encode('utf-8'))
     print("get lasers")
     lasers = []
-    for i in range(5):
+    for i in range(7):
         while serSTM.in_waiting == 0:
             time.sleep(0.002)
         line = float((serSTM.readline().decode('utf-8').rstrip()))
-        print(laserName[i] + ": " + str(line))
         lasers.append(line)
+        print(laserName[i] + ": " + lasers[i])
     return lasers
 
 if __name__ == '__main__':
